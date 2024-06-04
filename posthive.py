efd558120df8ba8e7f63c941dbf4c74cf01aa426
Ok-michael <okomichaelakpu2@gmail.com>
2024-06-04 23:21:03 +0100

    make several changes on the UI

--- posthive.py
+++ posthive.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask('__name__')
+
+app.config['SECRET_KEY'] = 'db86aff78e3b5089b0ddbfd20d9d14ae'
 
 posts = [
     {
@@ -32,6 +35,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='about')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form) 
+    
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
