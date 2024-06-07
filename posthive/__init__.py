cdd21b0e5ab52577145df5b95d07aebe6cf68f1b
Ok-michael <okomichaelakpu2@gmail.com>
2024-06-07 08:40:49 -0700

    commit before rollback

--- posthive/__init__.py
+++ posthive/__init__.py
@@ -7,4 +7,4 @@ app.config['SECRET_KEY'] = 'db86aff78e3b5089b0ddbfd20d9d14ae'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-from posthive import ro
+from posthive import routes
